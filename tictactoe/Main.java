1240fad11769897b31cad4f5dfa09c3b285caf76
Sergio Sanz Cebollero <scebollero@guidewire.com>
2020-11-05 09:24:42 +0100

    Tic-Tac-Toe project Stage 2 https://hyperskill.org/projects/48/stages/256/implement

--- tictactoe/Main.java
+++ tictactoe/Main.java
@@ -1,9 +1,18 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X X");
-        System.out.println("X O O");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter cells: ");
+        var gameState = scanner.nextLine();
+
+        System.out.println("---------");
+        System.out.printf("| %c %c %c |%n", gameState.charAt(0), gameState.charAt(1), gameState.charAt(2));
+        System.out.printf("| %c %c %c |%n", gameState.charAt(3), gameState.charAt(4), gameState.charAt(5));
+        System.out.printf("| %c %c %c |%n", gameState.charAt(6), gameState.charAt(7), gameState.charAt(8));
+        System.out.println("---------");
     }
 }
