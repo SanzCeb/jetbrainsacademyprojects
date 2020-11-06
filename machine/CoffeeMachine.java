f6003889bd4325bdbc53422eb8025d3e263d7a66
Sergio Sanz Cebollero <scebollero@guidewire.com>
2020-11-06 21:36:24 +0100

    Project Coffee Machine Stage 6 Completed. Project Completed https://hyperskill.org/projects/33/stages/180/implement

--- machine/CoffeeMachine.java
+++ machine/CoffeeMachine.java
@@ -1,31 +1,105 @@
 package machine;
 
 import machine.products.*;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    final static Scanner STDIN = new Scanner(System.in);
-    final static CoffeeMachine COFFEE_MACHINE = new CoffeeMachine();
-    private CoffeeMaker coffeeMaker = new CoffeeMaker(540, 400, 120, 9);
+    private final CoffeeMaker coffeeMaker = new CoffeeMaker(540, 400, 120, 9);
     private int money = 550;
+    private CoffeeMachineState state = CoffeeMachineState.IDLE;
+
+    String turnOn() {
+        state = CoffeeMachineState.IDLE;
+        return String.format("%s%n",state.getResponse());
+    }
+
+    String input(String userInput) {
+        String message;
+        switch (state) {
+            case IDLE:
+                message = doAction(userInput);
+                break;
+            case SELLING:
+                message = trySellingCoffee(userInput);
+                break;
+            case FILLING_WATER:
+                message = fillWater(userInput);
+                break;
+            case FILLING_MILK:
+                message = fillMilk(userInput);
+                break;
+            case ADDING_COFFEE_BEANS:
+                message = addCoffeeBeans(userInput);
+                break;
+            default:
+                message = addDisposableCups(userInput);
+                break;
+        }
+        return message;
+    }
+
+    private String addCoffeeBeans(String userInput) {
+        coffeeMaker.addCoffeBeans(Integer.parseInt(userInput));
+        state = CoffeeMachineState.FILLING_CUPS;
+        return String.format("%s%n",state.getResponse());
+    }
+
+    private String addDisposableCups(String action) {
+        coffeeMaker.addCups(Integer.parseInt(action));
+        state = CoffeeMachineState.IDLE;
+        return String.format("%s%n",state.getResponse());
+    }
+
+    private String trySellingCoffee(String action) {
+        var optionalCoffeeRecipe = readRecipe(action);
+        String message;
+        if (optionalCoffeeRecipe.isPresent()) {
+            message = tryBuyCoffee(optionalCoffeeRecipe.get());
+        } else {
+            state = CoffeeMachineState.IDLE;
+            message = String.format("%n%s%n",state.getResponse());
+        }
+        return message;
+    }
+
+    private Optional<CoffeeRecipe> readRecipe(String input) {
+        CoffeeRecipe coffeeRecipe;
+        switch (input) {
+            case "1":
+                coffeeRecipe = new Espresso();
+                break;
+            case "2":
+                coffeeRecipe = new Latte();
+                break;
+            case "3":
+                coffeeRecipe = new Cappucino();
+                break;
+            default:
+                coffeeRecipe = null;
+                break;
+        }
+        return Optional.ofNullable(coffeeRecipe);
+    }
+
 
     public String askMoney() {
-        return String.format("$%d of money", money);
+        return String.format("$%d of money%n", money);
     }
-    public String askRemainingMilk() {
-        return String.format("%d of milk", coffeeMaker.getMilkML());
-    }
-
-    public String askRemainingWater() {
-        return String.format("%d of water", coffeeMaker.getWaterML());
+    public String queryRemainingMilk() {
+        return String.format("%d of milk%n", coffeeMaker.getMilkML());
     }
 
-    public String askRemainingCoffeeBeans() {
-        return String.format("%d of coffee beans", coffeeMaker.getCoffeeBeansG());
+    public String queryRemainingWater() {
+        return String.format("%d of water%n", coffeeMaker.getWaterML());
     }
 
-    public String askRemainingCups() {
-        return String.format("%d of disposable cups", coffeeMaker.getDisposableCups());
+    public String queryCoffeeBeans() {
+        return String.format("%d of coffee beans%n", coffeeMaker.getCoffeeBeansG());
+    }
+
+    public String queryRemainingDisposableCups() {
+        return String.format("%d of disposable cups%n", coffeeMaker.getDisposableCups());
     }
 
     String tryBuyCoffee(CoffeeRecipe coffeeRecipe) {
@@ -33,7 +107,8 @@ public class CoffeeMachine {
         if (response == CoffeeMakerResponse.ENOUGH_RESOURCES) {
             money += coffeeRecipe.price();
         }
-        return  response.toString();
+        state = CoffeeMachineState.IDLE;
+        return  String.format("%s%n%s%n",response, state.getResponse());
     }
 
     private int takeMoney() {
@@ -42,100 +117,74 @@ public class CoffeeMachine {
         return moneyTaken;
     }
 
-    private void addCups(int cups) {
-        coffeeMaker.addCups(cups);
+    private String fillMilk(String milkML) {
+        coffeeMaker.fillMilk(Integer.parseInt(milkML));
+        state = CoffeeMachineState.ADDING_COFFEE_BEANS;
+        return String.format("%s%n",state.getResponse());
     }
 
-    private void addCoffeeBeans(int coffeeBeansG) {
-        coffeeMaker.addCoffeBeans(coffeeBeansG);
+    private String fillWater(String waterML) {
+        coffeeMaker.fillWater(Integer.parseInt(waterML));
+        state = CoffeeMachineState.FILLING_MILK;
+        return String.format("%s%n",state.getResponse());
     }
 
-    private void fillMilk(int milkML) {
-        coffeeMaker.fillMilk(milkML);
+
+    private String doAction(String action) {
+        String message;
+        switch (action) {
+            case "buy":
+                message = actionBuy();
+                break;
+            case "fill":
+                message = actionFill();
+                break;
+            case "take":
+                message = actionTake();
+                break;
+            default:
+                message = collectCoffeeMachineStats();
+                break;
+        }
+        return message;
     }
 
-    private void fillWater(int waterML) {
-        coffeeMaker.fillWater(waterML);
+    private String actionBuy() {
+        state = CoffeeMachineState.SELLING;
+        return String.format("%n%s%n", state.getResponse());
     }
 
+
+    private String actionTake() {
+        return String.format("%nI gave you $%d%n%s%n", takeMoney(), state.getResponse());
+    }
+
+    private String actionFill() {
+        state = CoffeeMachineState.FILLING_WATER;
+        return String.format("%n%s%n", state.getResponse());
+    }
+
+    private String collectCoffeeMachineStats() {
+        String outputFormat = "%nThe coffee machine has:%n%s%s%s%s%s%s%n";
+        var remainingWater = queryRemainingWater();
+        var remainingMilk = queryRemainingMilk();
+        var remainingCoffeeBeans = queryCoffeeBeans();
+        var remainingDisposableCups = queryRemainingDisposableCups();
+        var money = askMoney();
+        return String.format(outputFormat, remainingWater, remainingMilk,
+                remainingCoffeeBeans, remainingDisposableCups, money, state.getResponse());
+    }
 
     public static void main(String[] args) {
+        var coffeeMachine = new CoffeeMachine();
+        var scanner = new Scanner(System.in);
+        String response = coffeeMachine.turnOn();
         String action;
         do{
-            System.out.println("\nWrite action (buy, fill, take, remaining, exit):");
-            action = STDIN.nextLine();
-            System.out.println();
-            runAction(action);
+            System.out.print(response);
+            action = scanner.nextLine();
+            response = coffeeMachine.input(action);
         }while (!action.equals("exit"));
     }
 
-    private static void runAction(String action) {
-        switch (action) {
-            case "buy":
-                actionBuy();
-                break;
-            case "fill":
-                actionFill();
-                break;
-            case "take":
-                actionTake();
-                break;
-            case "remaining":
-                printCoffeeMachineStats();
-            default:
-                break;
-        }
-    }
-
-    private static void actionTake() {
-        int money = COFFEE_MACHINE.takeMoney();
-        System.out.printf("I gave you $%d%n", money);
-    }
-
-    private static void actionFill() {
-        System.out.println("Write how many ml of water do you want to add:");
-        var waterML = STDIN.nextInt();
-        System.out.println("Write how many ml of milk do you want to add:");
-        var milkML = STDIN.nextInt();
-        System.out.println("Write how many grams of coffee beans do you want to add:");
-        var coffeeBeansG = STDIN.nextInt();
-        System.out.println("Write how many disposable cups do you want to add:");
-        var cups = STDIN.nextInt();
-        STDIN.nextLine();
-        COFFEE_MACHINE.fillWater(waterML);
-        COFFEE_MACHINE.fillMilk(milkML);
-        COFFEE_MACHINE.addCoffeeBeans(coffeeBeansG);
-        COFFEE_MACHINE.addCups(cups);
-    }
-
-
-
-    private static void actionBuy() {
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
-        var choice = STDIN.nextLine();
-        System.out.println();
-        switch (choice) {
-            case "1":
-                System.out.println(COFFEE_MACHINE.tryBuyCoffee(new Espresso()));
-                break;
-            case "2":
-                System.out.println(COFFEE_MACHINE.tryBuyCoffee(new Latte()));
-                break;
-            case "3":
-                System.out.println(COFFEE_MACHINE.tryBuyCoffee(new Cappucino()));
-                break;
-            case "back":
-            default:
-                break;
-        }
-    }
-
-    private static void printCoffeeMachineStats() {
-        System.out.println("The coffee machine has:");
-        System.out.println(COFFEE_MACHINE.askRemainingWater());
-        System.out.println(COFFEE_MACHINE.askRemainingMilk());
-        System.out.println(COFFEE_MACHINE.askRemainingCoffeeBeans());
-        System.out.println(COFFEE_MACHINE.askRemainingCups());
-        System.out.println(COFFEE_MACHINE.askMoney());
-    }
 }
