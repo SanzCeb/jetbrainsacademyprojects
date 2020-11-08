32a61678b57cf9dd6af90dac28772207ee52fb2e
Sergio Sanz Cebollero <scebollero@guidewire.com>
2020-11-08 09:25:18 +0100

    Bulls And Cows Stage 3 Completed. https://hyperskill.org/projects/53/stages/289/implement

--- bullscows/Main.java
+++ bullscows/Main.java
@@ -3,13 +3,36 @@ package bullscows;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
-        var secretCode = new char[]{'9', '3', '0', '5'};
-        var answer = scanner.nextLine();
-        var bulls = 0;
-        var cows = 0;
+        long pseudoRandomNumber = System.nanoTime();
+        var secretCodeLength = scanner.nextInt();
+        StringBuilder sb = new StringBuilder();
 
+        if (secretCodeLength > 10) {
+            System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+        } else {
+            var reversedNumber = sb.append(pseudoRandomNumber).reverse();
+            System.out.printf("The random secret number is %s%n", secretCodeWithUniqueDigits(reversedNumber, secretCodeLength));
+        }
+    }
+
+    static StringBuilder secretCodeWithUniqueDigits(StringBuilder reversedNumber, int secretCodeLength) {
+        StringBuilder secretCodeBuilder = new StringBuilder();
+        for (int i = 0; i < reversedNumber.length() && secretCodeBuilder.length() < secretCodeLength; i++) {
+            var numberI = reversedNumber.charAt(i);
+            if ( !secretCodeBuilder.chars().anyMatch(digit -> digit == numberI)) {
+                secretCodeBuilder.append(numberI);
+            }
+        }
+        return secretCodeBuilder;
+    }
+
+    public static void bullsAndCows() {
+        var secretCode = new char[]{'9', '3', '0', '5'};
+        var answer = new Scanner(System.in).nextLine();
+        int bulls = 0, cows = 0;
         for (int i = 0; i < secretCode.length; i++) {
             if (secretCode[i] == answer.charAt(i)) {
                 bulls++;
@@ -17,6 +40,7 @@ public class Main {
                 cows++;
             }
         }
+
         if (bulls > 0) {
             if (cows > 0) {
                 System.out.printf("Grade: %d bull(s) and %d cows(s). The secret code is 9305.", bulls, cows);
@@ -28,6 +52,5 @@ public class Main {
         } else {
             System.out.printf("Grade: None. The secret code is 9305.");
         }
-
     }
 }
